356f000f26dfd183b5541d57420948d9e37958c2
Leon Si <leon@leonsilicon.com>
2025-10-16 03:03:35 -0400

    bundle

--- dist/dprint-exec-reexport.mjs
+++ dist/dprint-exec-reexport.mjs
@@ -250,12 +250,14 @@ if (moduleType === "module") {
 	newFileLines.push(...fileLines.slice(0, dprintReexportLineIndex), dprintReexportLine);
 	const matchedFiles = fs$1.globSync(path.resolve(dirpath, globPattern));
 	if (globfileType === "filepaths") newFileLines.push("export default {");
-	for (const matchedFile of matchedFiles) if (globfileType === "matches") {
-		let importSpecifier$1 = path.relative(dirpath, matchedFile);
-		if (!importSpecifier$1.startsWith(".")) importSpecifier$1 = `./${importSpecifier$1}`;
-		newFileLines.push(`export * from ${JSON.stringify(importSpecifier$1)};`);
-	} else if (globfileType === "files") newFileLines.push(`export * as ${JSON.stringify(path.relative(monorepoDirpath, matchedFile))} from ${JSON.stringify(importSpecifier)};`);
-	else if (globfileType === "filepaths") newFileLines.push(`${JSON.stringify(path.relative(monorepoDirpath, matchedFile))}: true,`);
+	for (const matchedFile of matchedFiles) {
+		let importSpecifier = path.relative(dirpath, matchedFile);
+		if (globfileType === "matches") {
+			if (!importSpecifier.startsWith(".")) importSpecifier = `./${importSpecifier}`;
+			newFileLines.push(`export * from ${JSON.stringify(importSpecifier)};`);
+		} else if (globfileType === "files") newFileLines.push(`export * as ${JSON.stringify(path.relative(monorepoDirpath, matchedFile))} from ${JSON.stringify(importSpecifier)};`);
+		else if (globfileType === "filepaths") newFileLines.push(`${JSON.stringify(path.relative(monorepoDirpath, matchedFile))}: true,`);
+	}
 	if (globfileType === "filepaths") newFileLines.push("}");
 	newFileLines.push(...fileLines.slice(dprintReexportEndLineIndex));
 } else throw new Error("Only module type is supported for now");
